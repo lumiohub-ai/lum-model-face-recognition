9c8909d75ed71b54e1f515143dfad8e2f581eff7
mirsaidl <abdurasulovm149@gmail.com>
2025-02-22 12:18:39 +0900

    Add main script for face recognition; implement video capture and processing; integrate tracking and logging features

---
+++ src/face_recognition/main.py
@@ -0,0 +1,108 @@
+
+from cfg import Config
+import cv2
+from engine import FaceRecognitionModel, TrackManager
+from utils import EntryLogger, generate_random_color, display
+import os
+
+def main(cfg):
+    cap = cv2.VideoCapture(cfg.video_path)
+    # Try increasing buffer size
+    cap.set(cv2.CAP_PROP_BUFFERSIZE, 3)
+
+    # Try setting a read timeout manually
+    cap.set(cv2.CAP_PROP_POS_MSEC, 5000)  # 5 seconds timeout
+
+    w, h, fps = (
+        int(cap.get(x))
+        for x in (cv2.CAP_PROP_FRAME_WIDTH, cv2.CAP_PROP_FRAME_HEIGHT, cv2.CAP_PROP_FPS)
+    )
+    out = cv2.VideoWriter(
+        cfg.output_video_path, cv2.VideoWriter_fourcc(*"mp4v"), fps, (714, 659)
+    )
+
+    stop = False
+    while True:
+        model.check_new_faces()
+
+        ret, frame = cap.read()
+        if not ret or stop:
+            break
+
+        x, y, w, h = cfg.camera_roi_coordinates
+        frame = frame[y : y + h, x : x + w]
+
+        detections = model.detector.track(
+            frame,
+            conf=cfg.detection_threshold,
+            verbose=False,
+            imgsz=cfg.imgsz,
+            persist=True,
+        )
+
+        if detections[0].boxes.id is None:
+            stop = display(frame, out)
+            continue
+
+        boxes = detections[0].boxes.data.cpu().tolist()
+        track_ids = detections[0].boxes.id.cpu().tolist()
+
+        for det, track_id in zip(boxes, track_ids):
+            x1, y1, x2, y2, _, _, _ = map(int, det)
+            h, w, _ = frame.shape
+            x1, y1, x2, y2 = max(0, x1), max(0, y1), min(w, x2), min(h, y2)
+
+            face = frame[y1:y2, x1:x2]
+            track.track_frame_count[track_id] += 1
+
+            if track.track_frame_count[track_id] >= cfg.check_interval:
+                track.name_to_track_id.pop(track_id, None)
+                track.track_frame_count[track_id] = 0
+
+            name = track.name_to_track_id.get(track_id, "Detecting...")
+
+            if name == "Detecting...":
+                face_emb = model.compute_embeddings(face)
+                name = model.recognize_face(face_emb)
+                if name != "Detecting...":
+                    track.name_to_track_id[track_id] = name
+
+            entry_logger.log_person_entry(name)
+
+            if name not in track.name_to_color:
+                track.name_to_color[name] = (
+                    (0, 0, 0) if name == "Detecting..." else generate_random_color()
+                )
+
+            color = track.name_to_color[name]
+            box_width = x2 - x1
+            font_scale = max(0.5, box_width / 300)
+            text_size = cv2.getTextSize(name, cv2.FONT_HERSHEY_DUPLEX, font_scale, 1)[0]
+            text_x = min(x2 - text_size[0] - 5, x1 + 5)
+            text_y = max(0, y1 - 10)
+
+            cv2.rectangle(frame, (x1, y1), (x2, y2), color, 3)
+            cv2.putText(
+                frame, name, (text_x, text_y), cv2.FONT_HERSHEY_DUPLEX, font_scale, (255, 255, 255), 2
+            )
+
+        entry_logger.visualize_entries(frame)
+
+        stop = display(frame, out)
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+
+    os.environ["OPENCV_FFMPEG_CAPTURE_OPTIONS"] = "rtsp_transport;udp|buffer_size;10485760"
+    os.environ["OPENCV_FFMPEG_CAPTURE_OPTIONS"] = "rtsp_transport;tcp"
+
+    cfg = Config()
+
+    model = FaceRecognitionModel(cfg.device, cfg.face_crops_path, cfg.match_threshold)
+    entry_logger = EntryLogger(cfg.logging_path)
+    track = TrackManager()
+
+    main(cfg)
