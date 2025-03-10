4482dac9df2c53928762da417ca46df75111fcae
mirsaidl <abdurasulovm149@gmail.com>
2025-03-10 09:00:29 +0900

    feat: add example script for HBFace face recognition; update .gitignore to exclude test.py

---
+++ examples/test.py
@@ -0,0 +1,11 @@
+import sys
+import os
+sys.path.append(os.curdir)
+
+from src.face_recognition.hbface import HBFace
+
+
+video_path = 'videos/output_11.mkv'
+streamer = HBFace(video_path=video_path, cam_type='IN', annot=True)
+
+streamer.run()
