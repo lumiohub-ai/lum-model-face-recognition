189ccc2284bee255b2028773310446313202a9a8
mirsaidl <abdurasulovm149@gmail.com>
2025-03-23 18:16:49 +0900

    fix: update video path and database path in test.py for improved functionality

--- examples/test.py
+++ examples/test.py
@@ -3,10 +3,10 @@ import os
 sys.path.append(os.curdir)
 from src.face_recognition.hbface import HBFace
 
-video_path = 'in.mp4'
+video_path = 'hb-videos/in.mp4'
 
-streamer = HBFace(video_path=video_path, cam_type='IN', show=True, db_path='data/hb-kor',
-                  match_threshold=0.6, detection_threshold=0.5, imgsz=1280, eval=False)
+streamer = HBFace(video_path=video_path, cam_type='IN', show=True, db_path='data/hb-kor-aug',
+                  match_threshold=0.1, detection_threshold=0.5, imgsz=1280, eval=False)
 
 streamer.run()
 
