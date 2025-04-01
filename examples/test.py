652c11260c3a4842d11ca335b93e1b73ec2c4c1f
mirsaidl <abdurasulovm149@gmail.com>
2025-04-01 14:32:45 +0900

    feat: ennabled showing content

--- examples/test.py
+++ examples/test.py
@@ -1,12 +1,19 @@
 import sys
 import os
-sys.path.append(os.curdir)
+
+# Set timezone to Asia/Tashkent
+os.environ['TZ'] = 'Asia/Tashkent'
+import time
+time.tzset()
+
 from datetime import datetime
+sys.path.append(os.curdir)
+
 from src.face_recognition.hbface import HBFace
 
 # RTSP streams for IN and OUT cameras
-in_camera = '' 
-out_camera = ''
+in_camera = 'rtsp://admin:Namhbai01@192.168.13.17:554'
+out_camera = 'rtsp://admin:Namhbai01@192.168.13.16:554'
 
 print("Starting face recognition...")
 print(f"Start time: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
@@ -18,9 +25,9 @@ face_engine_multi = HBFace(
     roi=[(383, 53, 1015, 709), (639, 1, 1276, 717)],
     line_points=[[(0, 273), (631, 264)], [(1, 2), (636, 712)]],
     multi_camera=True,
-    show=False,  # Disable display, just process and save
+    show=True,  # Disable display, just process and save
     match_threshold=0.6,
-    detection_threshold=0.5,
+    detection_threshold=0.7,
     imgsz=1280,
     padding_ratio=0.2,
     db_path='data/hb-uzb',
