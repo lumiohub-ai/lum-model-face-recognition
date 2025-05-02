10e3bb2947eb9c03d85b0abb926a0a9200067730
mirsaidl <abdurasulovm149@gmail.com>
2025-05-03 08:22:34 +0900

    feat: add multi-camera face recognition setup for Ilhan and Korea clients

---
+++ clients/ilhan.py
@@ -0,0 +1,20 @@
+from face_recognition import HBFace
+
+# RTSP streams for IN and OUT cameras
+in_camera = 'rtsp://admin:qwerty12.@192.168.1.68/Streaming/Channels/101'
+out_camera ='rtsp://admin:qwerty12.@192.168.1.69/Streaming/Channels/101'
+
+# Initialize HBFace for multi-camera setup
+face_engine_multi = HBFace(
+    cam_types=["IN","OUT"],
+    video_path=[in_camera, out_camera],
+    roi=[(994, 110, 1914, 962), (820, 87, 1703, 945)],
+    # line_points = [[(2217, 886), (941, 912)],[(609, 558), (1618, 601)]],
+    multi_camera=True,
+    show=False,  # Disable display, just process and save
+    log_file='logs/ilhan.log',
+    debug=True)
+
+
+# Run the face recognition system
+face_engine_multi.run()
--- clients/korea.py
+++ clients/korea.py
@@ -1,16 +1,18 @@
 from face_recognition import HBFace
 
 # RTSP streams for IN and OUT cameras
-in_camera = 'hb-videos/in.mp4' 
-out_camera = 'hb-videos/out.mp4'
+in_camera = 'rtsp://admin:bHfthUmGVXxtuXTu@192.168.217.151:554' 
+out_camera = 'rtsp://admin:hbai2024@192.168.217.150:554'
 
 # Initialize HBFace for multi-camera setup
 face_engine_multi = HBFace(
     cam_types=["IN", "OUT"],
     video_path=[in_camera, out_camera],
+    roi=[(383,53,1015,709), (639,1,1276,717)], 
     multi_camera=True,
     show=False,  # Disable display, just process and save
     match_threshold=0.3,
+    log_file='logs/korea.log',
     db_path='data/embeddings/hb-kor-camera.pkl',
 )
 
