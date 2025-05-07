8992383fdb69ff4f500d2b7160904ddcc851a6bc
mirsaidl <abdurasulovm149@gmail.com>
2025-05-07 16:25:32 +0900

    Update setup script: change yolo_tracking installation path to modules directory and add insightface module installation

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,5 +3,7 @@ git submodule update --init --recursive
 python3 -m venv myenv
 source myenv/bin/activate
 
-pip install -e yolo_tracking
+pip install -e modules/yolo_tracking
+pip install -e modules/insightface
+
 pip install -e .
