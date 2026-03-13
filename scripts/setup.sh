44cb4c1192d186fbc4aa6b94b4f67a71841cf343
azamjon-xusanov <azamjonxusanov.hbai@gmail.com>
2026-03-13 15:31:42 +0900

    refactor: remove unused scripts/files

--- scripts/setup.sh
+++
@@ -1,9 +0,0 @@
-git submodule update --init --recursive
-
-python3 -m venv myenv
-source myenv/bin/activate
-
-pip install -e modules/yolo_tracking
-pip install -e modules/insightface
-
-pip install -e .
